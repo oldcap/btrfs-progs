8a997eaee7a9f22735ccaa31b4810057e76b57a3
oldcap <zhe.zhang.research@gmail.com>
2014-07-02 16:56:41 -0400

    adding more blocks

--- btrfs-compose.c
+++ btrfs-compose.c
@@ -105,6 +105,11 @@ static int do_compose(const char *devname, const char *filename,
 
 	btrfs_set_inode_generation(leaf, inode, 7);
 	fprintf(stdout, "new generation is %llu\n", btrfs_inode_generation(leaf, inode));
+
+	ret = btrfs_record_file_extent(trans, root, objectid, inode, total_bytes,
+					104857600, 1048576);
+	btrfs_set_inode_nbytes(leaf, total_bytes + 1048576);
+
 	btrfs_commit_transaction(trans, root);
 
 	btrfs_release_path(&path);
