0ddb4ce723ae0b104466b28c39cfe10d99f7723c
oldcap <zhe.zhang.research@gmail.com>
2014-07-02 16:03:08 -0400

    fix bug

--- btrfs-compose.c
+++ btrfs-compose.c
@@ -101,6 +101,8 @@ static int do_compose(const char *devname, const char *filename,
 	leaf = path.nodes[0];
 	inode = btrfs_item_ptr(leaf, path.slots[0], struct btrfs_inode_item);
 	total_bytes = btrfs_inode_size(leaf, inode);
+
+	fprintf(stdout, "total size %llu\n", total_bytes);
 	btrfs_release_path(&path);
 
 
