26ac9a9f06a68ffbb8f0aec16a258c1cecc566ce
oldcap <zhe.zhang.research@gmail.com>
2014-07-03 15:53:54 -0400

    debug

--- btrfs-compose.c
+++ btrfs-compose.c
@@ -48,7 +48,7 @@ static int do_compose(const char *devname, const char *filename,
 	
 	fprintf(stdout, "Creating file %s\n", filename);
 	char *harddevname = "/dev/sdb";
-	char *hardfilename = "composed-file";
+	char *hardfilename = "vm-image";
 	// char *hardfilename = "auth.log";
 
 	// int fd = open(filename, O_CREAT, O_SYNC);
