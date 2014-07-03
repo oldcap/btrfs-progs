c2cfa7f47622dd78c77814ca7c6c3901dcafd127
oldcap <zhe.zhang.research@gmail.com>
2014-07-03 11:18:43 -0400

    debugging

--- btrfs-compose.c
+++ btrfs-compose.c
@@ -109,8 +109,9 @@ static int do_compose(const char *devname, const char *filename,
 	btrfs_set_inode_generation(leaf, inode, 7);
 	// btrfs_set_inode_nbytes(leaf, inode, total_bytes + 1048576);
 	fprintf(stdout, "new generation is %llu\n", btrfs_inode_generation(leaf, inode));
-	ret = btrfs_record_file_extent(trans, root, key.objectid, inode, total_bytes,
-					1048576, 1048576);
+	fprintf(stdout, "objectid is %d\n", key.objectid);
+	// ret = btrfs_record_file_extent(trans, root, key.objectid, inode, total_bytes,
+	// 				1048576, 1048576);
 	btrfs_set_inode_size(leaf, inode, total_bytes + 1048576);
 	total_bytes = btrfs_inode_size(leaf, inode);
 	fprintf(stdout, "new total size %llu\n", total_bytes);	
