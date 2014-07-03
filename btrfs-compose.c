8b37160e223a5d5c98a737f9fc4c2c4241cb71b1
oldcap <zhe.zhang.research@gmail.com>
2014-07-02 23:35:30 -0400

    extent

--- btrfs-compose.c
+++ btrfs-compose.c
@@ -107,13 +107,13 @@ static int do_compose(const char *devname, const char *filename,
 	fprintf(stdout, "old total size %llu\n", total_bytes);
 	fprintf(stdout, "old generation is %llu\n", btrfs_inode_generation(leaf, inode));
 	btrfs_set_inode_generation(leaf, inode, 7);
-	btrfs_set_inode_size(leaf, inode, total_bytes + 1048576);
 	// btrfs_set_inode_nbytes(leaf, inode, total_bytes + 1048576);
 	fprintf(stdout, "new generation is %llu\n", btrfs_inode_generation(leaf, inode));
-	total_bytes = btrfs_inode_size(leaf, inode);
-	fprintf(stdout, "new total size %llu\n", total_bytes);	
 	ret = btrfs_record_file_extent(trans, root, key.objectid, inode, total_bytes,
 					10485760, 1048576);
+	btrfs_set_inode_size(leaf, inode, total_bytes + 1048576);
+	total_bytes = btrfs_inode_size(leaf, inode);
+	fprintf(stdout, "new total size %llu\n", total_bytes);	
 	btrfs_mark_buffer_dirty(leaf);
 	btrfs_release_path(&path);
 	ret = btrfs_commit_transaction(trans, root);
